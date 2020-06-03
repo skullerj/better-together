f53523423cf5822a2b14d6fce99dbbddb8cadedf
Juan Rosero <jfrosero@udlanet.ec>
2020-06-02 22:10:12 -0500

    Allow users to see their donations

--- src/api/donation.js
+++ src/api/donation.js
@@ -4,14 +4,32 @@ import 'firebase/auth';
 
 export async function createDonation(values, user) {
   const firestore = firebase.firestore();
-  console.log(user);
   try {
     await firestore.collection('donations').add({
+      description: values.description,
       location: values.location,
       donnerId: user.uid,
       donner: user.displayName,
       donnerEmail: user.email,
-      status: 'registered'
+      status: 'registered',
+      createdAt: new Date()
+    });
+  } catch (e) {
+    throw new Error('Hubo un error creando la donación');
+  }
+}
+
+export async function getMyDonations(uid) {
+  const firestore = firebase.firestore();
+  try {
+    const donationsSnaps = await firestore
+      .collection('donations')
+      .where('donnerId', '==', uid)
+      .orderBy('createdAt', 'desc')
+      .get();
+    return donationsSnaps.docs.map(doc => {
+      const d = doc.data();
+      return { id: doc.id, ...d, createdAt: d.createdAt.toDate() };
     });
   } catch (e) {
     console.log(e);
--- src/pages/MyDonationsPage.jsx
+++ src/pages/MyDonationsPage.jsx
@@ -1,5 +1,100 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Heading, Text, Paragraph, Button } from 'grommet';
+import Loading from 'components/Loading';
+import { useAuth } from 'containers/AuthProvider';
+import { getMyDonations } from 'api/donation';
+import { format } from 'date-fns';
 
 export default function MyDonationsPage() {
-  return <h1>Aquí van las donaciones</h1>;
+  const { user } = useAuth();
+  const [loading, setLoading] = useState(true);
+  const [donations, setDonations] = useState([]);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    async function loadDonations() {
+      try {
+        const donations = await getMyDonations(user.uid);
+        console.log(donations);
+        setDonations(donations);
+      } catch (e) {
+        setError(e.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadDonations();
+  }, [user.uid]);
+
+  return (
+    <Box flex="grow" overflow="auto" pad="medium">
+      {loading && (
+        <Box align="center" margin={{ top: 'medium' }}>
+          <Loading />
+        </Box>
+      )}
+      {!loading && (
+        <Box>
+          <Heading level="3" margin={{ vertical: 'small' }} textAlign="center">
+            Mis Donaciones
+          </Heading>
+          {donations.length === 0 && (
+            <Text color="text-xweak" margin={{ top: 'medium' }}>
+              No has realizado ninguna donación todavía
+            </Text>
+          )}
+          {donations.map(d => (
+            <Box
+              margin={{ top: 'medium' }}
+              elevation="small"
+              pad="medium"
+              background="background-front"
+            >
+              <Text size="xsmall" textAlign="end">
+                {format(d.createdAt, 'dd-MM-yyyy')}
+              </Text>
+              <Heading level="4" margin={{ bottom: 'small', top: '0' }}>
+                {d.location.name}
+              </Heading>
+              <Paragraph color="text-xweak">{d.description}</Paragraph>
+              {d.status === 'registered' && (
+                <Text size="small" color="text-xweak">
+                  Esta donación está registrada. Por favor acércate al centro de
+                  acopio a entregar los productos
+                </Text>
+              )}
+              {d.status === 'received' && (
+                <Text size="small" color="status-warning">
+                  Hemos recibido tu donación. En pronto actualizaremos los datos
+                  del beneficiario.
+                </Text>
+              )}
+              {d.status === 'delivered' && (
+                <Text size="small" color="status-ok">
+                  Hemos entregado esta donación. Gracias por ayudar!
+                </Text>
+              )}
+            </Box>
+          ))}
+        </Box>
+      )}
+      {!loading && error && (
+        <Box>
+          <Heading level="3" margin={{ vertical: 'small' }} textAlign="center">
+            Mis Donaciones
+          </Heading>
+          <Box
+            margin={{ top: 'medium' }}
+            elevation="small"
+            pad="medium"
+            background="background-front"
+          >
+            <Text color="status-critical" margin={{ bottom: 'medium' }}>
+              Hubo un error. Por favor intenta más tarde
+            </Text>
+          </Box>
+        </Box>
+      )}
+    </Box>
+  );
 }
